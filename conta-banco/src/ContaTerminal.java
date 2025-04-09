b710e3a7f45b17a5511a6f7f2366d93ada93adbd
zaethyr <emaildonatan1@gmail.com>
2025-04-09 17:44:43 -0300

    Codigo corrigido pra melhor visulização.

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
         // importando a classe Scanner para ler os dados do usuário
+        try (Scanner teclado = new Scanner(System.in)) {
+            System.out.println("Por favor, digite o número da Agência!");
+            String agencia = teclado.nextLine();
 
-        Scanner teclado = new Scanner(System.in);
-        System.out.println("Por favor, digite o número da Agência !");
-        String agencia = teclado.nextLine();
-        System.out.println("Por favor, digite o número da Conta !");
-        int numeroConta = teclado.nextInt();
-        System.out.println("Por favor, digite o seu nome !");
-        String nomeCliente = teclado.next();
-        System.out.println("Por favor, digite o saldo da conta !");
-        double saldo = teclado.nextDouble();
+            System.out.println("Por favor, digite o número da Conta!");
+            int numeroConta = teclado.nextInt();
+            teclado.nextLine();
 
-        // Exibindo os dados do cliente
+            System.out.println("Por favor, digite o seu nome!");
+            String nomeCliente = teclado.nextLine();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
-                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+            System.out.println("Por favor, digite o saldo da conta!");
+            double saldo = teclado.nextDouble();
+
+            // Exibindo os dados do cliente
+            System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                    + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+        }
     }
 }

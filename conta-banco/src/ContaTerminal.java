e7016bbe6f08120b31966fec04427ee7c5e0f5ff
zaethyr <emaildonatan1@gmail.com>
2025-04-09 15:46:12 -0300

    Implementa leitura de dados do usuário para criação de conta bancária

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // importando a classe Scanner para ler os dados do usuário
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Agência !");
+        String agencia = teclado.nextLine();
+        System.out.println("Por favor, digite o número da Conta !");
+        int numeroConta = teclado.nextInt();
+        System.out.println("Por favor, digite o seu nome !");
+        String nomeCliente = teclado.next();
+        System.out.println("Por favor, digite o saldo da conta !");
+        double saldo = teclado.nextDouble();
+
+        // Exibindo os dados do cliente
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é "
+                + agencia + ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
